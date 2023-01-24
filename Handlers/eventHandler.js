5732fbbba9daec010b571dfa0ecc864cd881fc83
Gile <mladen.reljic87@gmail.com>
2023-01-24 22:10:05 +0100

    Add loadFiles function that deletes cache version of files

---
+++ Handlers/eventHandler.js
@@ -0,0 +1,5 @@
+async function loadEvents(client) {
+  const { loadFiles } = require('../Functions/fileLoader.js');
+  const ascii = require('ascii-table');
+  const table = new ascii('Events', 'Status');
+}
